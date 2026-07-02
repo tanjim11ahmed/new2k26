b9acae1b4c07d94910e4ed13633351a6dd10d9ad
Tanjim Ahmed <tanjimahmedpk28@gmail.com>
2026-07-02 08:27:49 +0600

    Add put endpoint for updating students

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
+
 
 @app.get("/")
 def home():
@@ -42,4 +43,53 @@ def about():
     return {
         "developer": "Tanjim Ahmed",
         "mission": "2 Crore in 2 Years"
+    }
+
+
+students = []
+from pydantic import BaseModel
+class student(BaseModel):
+    name: str
+    department: str
+    age: int
+
+
+@app.post("/student")
+def create_student(student: student):
+    students.append(student)
+
+    return {
+        "Message": " Student added successfully"
+    }
+@app.get("/students")
+def get_students():
+    return students
+       
+@app.put("/student/{id}")
+def update_student(id: int, student: student):
+
+    if id >= len(students):
+        raise HTTPException(
+            status_code=404,
+            detail="Student not found"
+        )
+
+    students[id] = student
+
+    return {
+        "message": "Student Updated Successfully"
+    }
+@app.delete("/student/{id}")
+def delete_student(id: int):
+
+    if id >= len(students):
+        raise HTTPException(
+            status_code=404,
+            detail="Student not found"
+        )
+
+    students.pop(id)
+
+    return {
+        "message": "Student Deleted Successfully"
     }
